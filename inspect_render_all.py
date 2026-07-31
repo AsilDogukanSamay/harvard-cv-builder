7a7a41bde66b683525a0086896bfa05d5d952965
Asil Dogukan Samay <dogukan__sam_ay@hotmail.com>
2026-07-31 14:06:21 +0300

    Clean: Remove temporary debug scripts and finalize clean input fixes

--- inspect_render_all.py
+++
@@ -1,8 +0,0 @@
-with open("app.js", "r", encoding="utf-8") as f:
-    js = f.read()
-
-lines = js.split("\n")
-print("=== renderAll function ===")
-for i in range(1406, 1490):
-    if i < len(lines):
-        print(f"Line {i+1}: {lines[i]}")
